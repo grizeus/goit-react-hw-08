17519772a1a6862b509e5577099b02fd16acd0a5
grizeus <32486511+grizeus@users.noreply.github.com>
2024-09-20 14:48:49 +0300

    Included Formik library (#3)
    * add nextID for ID generation
    
    * setup formic logic

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,21 +1,53 @@
+import css from "./ContactForm.module.css";
+import { useId } from "react";
+import { Formik, Field, Form, ErrorMessage } from "formik";
+import * as Yup from "yup";
+import nextId from "react-id-generator";
+
+const initialContact = {
+  name: "",
+  number: "",
+};
+
+const validationSchema = Yup.object().shape({
+  name: Yup.string()
+    .min(3, "Too short!")
+    .max(50, "Too long!")
+    .required("Required"),
+  number: Yup.string()
+    .min(5, "Too short!")
+    .max(13, "Too long!")
+    .required("Required"),
+});
+
 const ContactForm = ({ onAdd }) => {
-  const handleSubmit = e => {
-    e.preventDefault();
+  const nameID = useId();
+  const numberID = useId();
+
+  const handleSubmit = (values, actions) => {
     onAdd({
-      id: Date.now(),
-      name: e.target.elements.name.value,
-      number: e.target.elements.number.value,
+      id: nextId(),
+      ...values,
     });
-    e.target.reset();
+    actions.reset();
   };
+
   return (
-    <form style={{ display: "flex", flexDirection: "column", width: "220px", gap: "10px" }} onSubmit={handleSubmit}>
-      <input type="text" name="name" placeholder="Name" />
-      <input type="text" name="number" placeholder="Phone number" />
-      <button type="submit">
-        Add
-      </button>
-    </form>
+    <Formik
+      initialValues={initialContact}
+      validationSchema={validationSchema}
+      onSubmit={handleSubmit}>
+      <Form className={css.form}>
+        <label htmlFor={nameID}>Name</label>
+        <Field type="text" name="name" placeholder="..." id={nameID} />
+        <ErrorMessage name="name" component="span" />
+
+        <label htmlFor={numberID}>Phone number</label>
+        <Field type="text" name="number" placeholder="..." id={numberID} />
+        <ErrorMessage name="number" component="span" />
+        <button type="submit">Add contact</button>
+      </Form>
+    </Formik>
   );
 };
 
