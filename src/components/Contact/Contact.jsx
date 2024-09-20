ca845aee179353eba2e8497e7f981b811d9f6098
grizeus <32486511+grizeus@users.noreply.github.com>
2024-09-20 18:07:06 +0300

    Icons (#6)
    * add some styles
    
    * replace id-gen with date manipulation stuff
    
    * add icons for contacts

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,11 +1,20 @@
 import css from "./Contact.module.css";
+import { MdPerson, MdPhone } from "react-icons/md";
 
 const Contact = ({ id, name, phone, handleDelete }) => {
   return (
     <div className={css.contact}>
-      <h2>{name}</h2>
-      <p>{phone}</p>
-      <button className={css["contact-btn"]} onClick={() => handleDelete(id)}>Delete</button>
+      <h3 className={css["contact-content"]}>
+        {" "}
+        <MdPerson className={css.icon} /> {name}
+      </h3>
+      <p className={css["contact-content"]}>
+        {" "}
+        <MdPhone className={css.icon} /> {phone}
+      </p>
+      <button className={css["contact-btn"]} onClick={() => handleDelete(id)}>
+        Delete
+      </button>
     </div>
   );
 };
