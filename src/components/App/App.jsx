c13c1d2cad4e2b03c1f824efa51d75ad7bc920d2
grizeus <zaytsewww@gmail.com>
2024-09-20 14:05:32 +0300

    add contacts storing in local storage

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,21 +1,31 @@
 // import css from './App.module.css'
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ContactForm from "../ContactForm/ContactForm";
 import ContactList from "../ContactList/ContactList";
 import SearchBox from "../SearchBox/SearchBox";
 import initialContacts from "../../data/contacts.json";
 
 function App() {
-  const [contacts, setContacts] = useState(initialContacts);
+  // use nullish coalescing to check if the stored contacts exist
+  const [contacts, setContacts] = useState(() => {
+    return (
+      JSON.parse(window.localStorage.getItem("stored-contacts")) ??
+      initialContacts
+    );
+  });
   const [search, setSearch] = useState("");
 
-  const deleteContact = (contactID) => {
+  useEffect(() => {
+    window.localStorage.setItem("stored-contacts", JSON.stringify(contacts));
+  }, [contacts]);
+
+  const deleteContact = contactID => {
     setContacts(prevContacts => {
       return prevContacts.filter(contact => contact.id !== contactID);
     });
   };
 
-  const addContact = (newContact) => {
+  const addContact = newContact => {
     setContacts(prevContacts => {
       return [...prevContacts, newContact];
     });
