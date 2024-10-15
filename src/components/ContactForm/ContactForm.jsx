f56c4424bd2d147dbad61dbd4bffea77f950bb51
grizeus <32486511+grizeus@users.noreply.github.com>
2024-10-15 16:05:22 +0300

    Redux store refactoring (#1)

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -3,7 +3,7 @@ import { Formik, Field, Form, ErrorMessage } from "formik";
 import * as Yup from "yup";
 
 import { useDispatch } from "react-redux";
-import { addContact } from "../../redux/contactsOps";
+import { addContact } from "../../redux/contacts/operations";
 
 import css from "./ContactForm.module.css";
 
