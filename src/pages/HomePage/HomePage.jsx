d7decf4c6f12a1da555ee46c80ce5370de138a57
grizeus <32486511+grizeus@users.noreply.github.com>
2024-10-23 00:42:15 +0300

    add toast on succes/error procedures (#8)

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,12 +1,15 @@
 import { useSelector } from "react-redux";
-import styles from "./HomePage.module.css";
-import { selectUser } from "../../redux/auth/selectors";
+import { Toaster } from "react-hot-toast";
+
 import ContactForm from "../../components/ContactForm/ContactForm";
+import { selectUser } from "../../redux/auth/selectors";
+import styles from "./HomePage.module.css";
 
 const HomePage = () => {
   const user = useSelector(selectUser);
   return (
     <section title="home page">
+      <Toaster position="top-right" toastOptions={{ duration: 3000 }} />
       <h1 className={styles.title}>
         Welcome, {user.name !== null ? user.name : "stranger"}!
       </h1>
