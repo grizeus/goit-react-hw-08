a96de1825582f875275bcdc2c76c629b08b9a320
grizeus <32486511+grizeus@users.noreply.github.com>
2024-10-25 14:19:51 +0300

    replace axios with instance, add patch operation to edit contacts (#14)

---
+++ src/api/api.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+const instanceContacts = axios.create({
+    baseURL: "https://connections-api.goit.global/",
+    headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET, POST, DELETE, PATCH",
+        "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
+    }
+});
+
+export default instanceContacts;
