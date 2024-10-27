4a576656bb11fd722f39c12d7390477a3c354852
grizeus <zaytsewww@gmail.com>
2024-10-27 16:37:50 +0200

    .

--- src/api/api.js
+++ src/api/api.js
@@ -3,10 +3,12 @@ import axios from "axios";
 const instanceContacts = axios.create({
   baseURL: "https://connections-api.goit.global/",
   headers: {
-    "Content-Type": "utf-8",
+    "Content-Type": "application/json",
     "Access-Control-Allow-Origin": "*",
     "Access-Control-Allow-Methods": "GET, POST, DELETE, PATCH",
-  }
+    "Access-Control-Allow-Headers":
+      "X-Requested-With, content-type, Authorization",
+  },
 });
 
 export default instanceContacts;
