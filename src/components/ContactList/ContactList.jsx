f3eea451d5ca8696de803226fe95ea02d6af95b5
grizeus <32486511+grizeus@users.noreply.github.com>
2024-11-08 21:24:18 +0200

    [Fix] Add empty contacts array handling(#20)

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,20 +1,26 @@
-import css from "./ContactList.module.css";
-import Contact from "../Contact/Contact";
 import { useSelector } from "react-redux";
+
+import Contact from "../Contact/Contact";
 import { selectFilteredContacts } from "../../redux/selectors";
+
+import styles from "./ContactList.module.css";
 
 const ContactList = () => {
   const filteredContacts = useSelector(selectFilteredContacts);
 
   return (
     <section title="Contact list">
-      <ul className={css["contact-list"]}>
-        {filteredContacts.map(({ id, name, number }) => (
-          <li key={id}>
-            <Contact id={id} name={name} number={number} />
-          </li>
-        ))}
-      </ul>
+      {Array.isArray(filteredContacts) && filteredContacts.length > 0 ? (
+        <ul className={styles["contact-list"]}>
+          {filteredContacts.map(({ id, name, number }) => (
+            <li key={id}>
+              <Contact id={id} name={name} number={number} />
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <span className={styles.notification}>No contacts in your phonebook yet</span>
+      )}
     </section>
   );
 };
